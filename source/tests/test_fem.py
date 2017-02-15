15744a558fb9cfc8e6b22084b1c20959dfc29561
Jan <jan.zmazek@gmail.com>
2017-02-15 12:09:47 +0100

    Wrote all tests.
    Everything OK.

--- source/tests/test_fem.py
+++ source/tests/test_fem.py
@@ -3,7 +3,7 @@ import numpy as np
 from source.fem import Fem
 
 
-class Test_fem(unittest.TestCase):
+class Test_Fem(unittest.TestCase):
     vertices_matrix = np.array(
         [[0., 0.],
         [0.5, 0.],
@@ -26,26 +26,24 @@ class Test_fem(unittest.TestCase):
         [4, 8, 7]])
 
     def test_local_assembly(self):
-        fem = Fem(Test_fem.vertices_matrix, Test_fem.connectivity_matrix)
+        fem = Fem(Test_Fem.vertices_matrix, Test_Fem.connectivity_matrix)
         fem.local_assembly([0,1,4], lambda x, y: 1, lambda x, y: 1)
         local_stiffness =[
             [ 0.5, -0.5, 0.],
             [-0.5, 1., -0.5],
             [ 0., -0.5, 0.5]]
+
         local_load = [
             [0.04166667],
             [0.04166667],
             [0.04166667]]
+
+        self.assertTrue((fem.local_stiffness == local_stiffness).all())
         for i in range(3):
-            for j in range(3):
-                self.assertAlmostEqual(
-                    fem.local_stiffness[i][j],
-                    local_stiffness[i][j])
-        for j in range(3):
             self.assertAlmostEqual(fem.local_load[i][0], local_load[i][0])
 
     def test_global_assembly(self):
-        fem = Fem(Test_fem.vertices_matrix, Test_fem.connectivity_matrix)
+        fem = Fem(Test_Fem.vertices_matrix, Test_Fem.connectivity_matrix)
         fem.global_assembly([0,1,2], [[1,1,1],[1,1,1],[1,1,1]], [[1],[1],[1]])
         global_stiffness = [
             [ 1., 1., 1., 0., 0., 0., 0., 0., 0.],
@@ -58,24 +56,15 @@ class Test_fem(unittest.TestCase):
             [ 0., 0., 0., 0., 0., 0., 0., 0., 0.],
             [ 0., 0., 0., 0., 0., 0., 0., 0., 0.]]
         global_load = [[ 1.],[ 1.],[ 1.],[ 0.],[ 0.],[ 0.],[ 0.],[ 0.],[ 0.]]
-        for i in range(9):
-            for j in range(9):
-                self.assertEqual(
-                    fem.global_stiffness[i][j],
-                    global_stiffness[i][j])
-        for j in range(9):
-            self.assertEqual(fem.global_load[i][0], global_load[i][0])
+        self.assertTrue((fem.global_stiffness == global_stiffness).all())
+        self.assertTrue((fem.global_load == global_load).all())
 
     def test_get_mapping_matrix(self):
         result = np.array([[0.5, 0.5],[0., 0.5]])
-        fem = Fem(Test_fem.vertices_matrix, Test_fem.connectivity_matrix)
-        for i in range(2):
-            for j in range(2):
-                self.assertEqual(
-                    fem.get_mapping_matrix([0, 1, 4])[i][j],
-                        result[i][j])
+        fem = Fem(Test_Fem.vertices_matrix, Test_Fem.connectivity_matrix)
+        self.assertTrue((fem.get_mapping_matrix([0, 1, 4]) == result).all())
 
     def test_get_boundary_array(self):
-        fem = Fem(Test_fem.vertices_matrix, Test_fem.connectivity_matrix)
+        fem = Fem(Test_Fem.vertices_matrix, Test_Fem.connectivity_matrix)
         boundary_array = [0, 1, 2, 3, 5, 6, 7, 8]
         self.assertListEqual(boundary_array, fem.get_boundary_array())
---
+++ source/tests/test_grid.py
@@ -0,0 +1,17 @@
+import unittest
+import numpy as np
+from source.grid import Grid
+
+
+class Test_Grid(unittest.TestCase):
+    static_file = "source/tests/fixtures/example.vtk"
+
+    def test_get_vertices_matrix(self):
+        grid = Grid(Test_Grid.static_file)
+        self.assertTrue((grid.get_vertices_matrix() == \
+            [[0, 0], [1, 0], [1, 1], [0, 1]]).all())
+
+    def test_get_connectivity_matrix(self):
+        grid = Grid(Test_Grid.static_file)
+        self.assertTrue((grid.get_connectivity_matrix() == \
+            [[0, 1, 2], [3, 0, 2]]).all())
