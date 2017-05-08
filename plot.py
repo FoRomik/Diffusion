e922f5e4e3dcabc3cb53f138c8e8bb0f32d4fb83
Jan <jan.zmazek@gmail.com>
2017-05-08 12:55:50 +0100

    Modifications on FDM solver, less files for FEM solver.

--- plot.py
+++ plot.py
@@ -4,14 +4,35 @@ This module computes finite element method and plots the result.
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
 from source.grid import Grid
 from source.square import SquareGrid
 from source.fem import Fem
+from source.fdm import Fdm
 
 
-def plot(vtk_file_name, sigma, function, integration_order=4):
+def plotFdm(m, n, hx, hy, sigma, dx_sigma, dy_sigma, function):
+    """
+    This function plots solution of a FDM method on input rectangle.
+    """
+    grid = Fdm(m, n, hx, hy)
+    solution = grid.solve(sigma, dx_sigma, dy_sigma, function)
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    X = np.arange(0, m)
+    Y = np.arange(0, n)
+    X, Y = np.meshgrid(X, Y)
+
+    # Plot the surface.
+    surf = ax.plot_surface(X, Y, solution, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False)
+    plt.show()
+
+def plotFem(vtk_file_name, sigma, function, integration_order=4):
     """
     This function plots solution of a FEM method on input grid.
     """
@@ -35,7 +56,7 @@ def plot(vtk_file_name, sigma, function, integration_order=4):
         cmap=plt.cm.seismic)
     plt.show()
 
-def plot_square(columns, rows, sigma, function, integration_order=4):
+def plotFem_square(columns, rows, sigma, function, integration_order=4):
     """
     This function plots solution of a FEM method on square generated grid.
     """
--- source/fdm.py
+++ source/fdm.py
@@ -1,12 +1,24 @@
 import numpy as np
 
-class FDM(object):
+class Fdm(object):
     """docstring for FDM."""
     def __init__(self, m, n, hx, hy):
         self.m = m
         self.n = n
         self.hx = hx
         self.hy = hy
+
+    def solve(self, sigma, dx_sigma, dy_sigma, function):
+        matrix_a = self.compute_a(sigma, dx_sigma, dy_sigma)
+        vector_b = self.compute_b(function)
+        partial_solution = np.linalg.solve(matrix_a, vector_b).flatten()
+        solution = np.zeros((self.m, self.n))
+        counter = 0
+        for i in range(1, self.m-1):
+            for j in range(1,self.n-1):
+                solution[i][j] = partial_solution[counter]
+                counter = counter + 1
+        return solution
 
     def compute_a(self, sigma, dx_sigma, dy_sigma):
         rank = (self.m-2)*(self.n-2)
@@ -16,22 +28,22 @@ class FDM(object):
                 x = (i%(self.m-2)+1)*self.hx
                 y = (i//(self.m-2)+1)*self.hy
                 if j == i:
-                    matrix_a[i][j] = -2*sigma(x,y) * (1/self.hx**2 + 1/self.hy**2)
+                    matrix_a[i][j] = 2*sigma(x,y) * (1/self.hx**2 + 1/self.hy**2)
                 elif j == i+1:
-                    matrix_a[i][j] = sigma(x,y)/self.hx**2 + dx_sigma(x,y)/(2*self.hx)
+                    matrix_a[i][j] = -sigma(x,y)/self.hx**2 - dx_sigma(x,y)/(2*self.hx)
                 elif j == i-1:
-                    matrix_a[i][j] = sigma(x,y)/self.hx**2 - dx_sigma(x,y)/(2*self.hx)
+                    matrix_a[i][j] = -sigma(x,y)/self.hx**2 + dx_sigma(x,y)/(2*self.hx)
                 elif j == i+self.m-2:
-                    matrix_a[i][j] = sigma(x,y)/self.hy**2 - dy_sigma(x,y)/(2*self.hy)
+                    matrix_a[i][j] = -sigma(x,y)/self.hy**2 + dy_sigma(x,y)/(2*self.hy)
                 elif j == i-self.m-2:
-                    matrix_a[i][j] = sigma(x,y)/self.hy**2 - dy_sigma(x,y)/(2*self.hy)
+                    matrix_a[i][j] = -sigma(x,y)/self.hy**2 + dy_sigma(x,y)/(2*self.hy)
         return matrix_a
 
-    def compute_b(self, f):
+    def compute_b(self, function):
         rank = (self.m-2)*(self.n-2)
         vector_b = np.zeros(rank)
         for i in range(rank):
             x = (i%(self.m-2)+1)*self.hx
             y = (i//(self.m-2)+1)*self.hy
-            vector_b[i] = f(x,y)
+            vector_b[i] = function(x,y)
         return vector_b
