e6ba3c07a2c969de21272c7565f6f4a9415a5e99
Jan <jan.zmazek@gmail.com>
2017-05-09 20:04:47 +0100

    Solve and Plot are new classes.
    for solving systems and plotting solutions

---
+++ source/plot.py
@@ -0,0 +1,44 @@
+"""
+This class of methods computes FDM/FEM and plots the result.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
+
+class Plot(object):
+    """docstring for Plot."""
+    def __init__(self, solution):
+        self.solution = solution
+
+    def FDM(self, X, Y):
+        """
+        This function plots solution of a FDM method on input rectangle.
+        """
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        X, Y = np.meshgrid(X, Y)
+
+        # Plot the surface.
+        surf = ax.plot_surface(X, Y, self.solution, cmap=cm.coolwarm,
+                               linewidth=0, antialiased=False)
+        plt.show()
+
+    def FEM(self, vertices_matrix, connectivity_matrix):
+        """
+        This function plots solution of a FEM method on input grid.
+        """
+
+        triangles = np.asarray(connectivity_matrix)
+
+        fig = plt.figure()
+        axis = fig.gca(projection='3d')
+        axis.plot_trisurf(
+            vertices_matrix[:, 0],
+            vertices_matrix[:, 1],
+            solution,
+            triangles=triangles,
+            cmap=plt.cm.seismic)
+        plt.show()
---
+++ source/solve.py
@@ -0,0 +1,9 @@
+import numpy as np
+import scipy as sp
+
+
+class Solve(object):
+    """docstring for Solve."""
+    def __init__(self, matrix_a, vector_b):
+        self.matrix_a = matrix_a
+        self.vector_b = vector_b
